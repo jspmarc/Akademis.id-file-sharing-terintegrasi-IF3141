6146abb000421f989d8c00c0f12d663035cedc34
Josep Marcello <jspmarcello@live.com>
2021-11-18 00:59:47 +0700

    fix: user is now able to register
    password is not hashed tho

--- src/models/User.py
+++ src/models/User.py
@@ -9,32 +9,32 @@ class User(models.Model):
     _name = 'filesharing.user'
     _description = 'User yang dapat mengakses sistem.'
 
-    user_id = fields.Many2one('res.users', required=True, invisible=True)
-    name = fields.Char(related="user_id.partner_id.name",
+    user_id = fields.Many2one('res.users', required=True)
+    name = fields.Char(related='user_id.name',
                        required=True, store=True)
-    email = fields.Char(related="user_id.partner_id.email",
-                        required=True, store=True)
-    password = fields.Char(related="user_id.password",
-                           required=True)
+    email = fields.Char(related='user_id.email', required=True, store=True)
+    password = fields.Char(
+        default=lambda self: self._generate_new_password()[0], readonly=True)
     jabatan = fields.Many2one('filesharing.jabatan', required=True)
     divisi = fields.Many2one('filesharing.divisi', required=True)
 
-    # def __generate_new_password(self):
-    #     '''
-    #     Fungsi untuk membuat sebuah password random baru
-    #     '''
-    #     rand = token_hex(6)
-    #     salt = self.__generate_salt()
-    #     password = rand + '$' + salt
+    def _generate_new_password(self):
+        '''
+        Fungsi untuk membuat sebuah password random baru
+        '''
+        rand = token_hex(6)
+        salt = self._generate_salt()
+        password = rand + '$' + salt
 
-    #     return (
-    #         password,
-    #         pbkdf2_hmac('sha256', bytearray(password, 'utf-8'),
-    #                     bytearray(salt, 'utf-8'), 1000).hex()
-    #     )
+        # return (
+        #     password,
+        #     pbkdf2_hmac('sha512', bytearray(password, 'utf-8'),
+        #                 bytearray(salt, 'utf-8'), 1000).hex()
+        # )
+        return rand
 
-    # def __generate_salt(self, n: int = 12) -> str:
-    #     return token_hex(n)
+    def _generate_salt(self, n: int = 12) -> str:
+        return token_hex(n)
 
     # def __set_new_password(self):
     #     '''
